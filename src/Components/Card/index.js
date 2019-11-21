14f20322fd42c00f56d59e9da8af1e666be071c7
Xelit-Emilien <emilienvandevelde@gmail.com>
2019-11-21 10:40:06 +0100

    fix card

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -7,8 +7,8 @@ import "./Card.scss"
 
 const Card = props => {
 
-    const isSelected = [props.selectHeroes.hero1, props.selectHeroes.hero2].some(hero => hero && hero.id === props.id);
     const {hero} = props
+    const isSelected = [props.selectHeroes.hero1, props.selectHeroes.hero2].some(selecthero => selecthero && selecthero.id === hero.id);
 
     return (
         <div className={`card ${hero ? hero.biography['publisher'] : 'disabled'}`}>
